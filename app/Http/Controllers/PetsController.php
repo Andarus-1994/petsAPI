33223d5b34eb360600f7971d898a44b488419f62
Andrei <andrei_king1@yahoo.com>
2021-05-21 11:34:30 +0300

    Testing the Loop for pets unicity

--- app/Http/Controllers/PetsController.php
+++ app/Http/Controllers/PetsController.php
@@ -171,9 +171,9 @@ $saveDetailsArray =[];
 
 $pageNumber= $request->page;
 
-
+foreach($allPets as $pet){
     foreach($petsArray as $petRequest){
-        foreach($allPets as $pet){
+   
         if(!empty($searchedPet)){
             
             if($petRequest['species']['id'] ===$pet['id'] && str_contains(strtolower($pet["name"]),strtolower($searchedPet))){
