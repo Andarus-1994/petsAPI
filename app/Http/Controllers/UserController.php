4e8b1ab16befebce1e3d394df471e3425d0e21f7
Andrei <andrei_king1@yahoo.com>
2021-06-23 09:52:20 +0300

    Fixed error message

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
+            return response()->json(['error'=>$validator->errors()->toJson()]);
         }
         
         $firebase = (new Factory)
