daa15653d0aaad0aa5e68d383106f527a4a1fa9b
Andrei <andrei_king1@yahoo.com>
2022-06-08 11:42:22 +0300

    import was missing firebase

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
 
 class FeedbackController extends Controller
 {
